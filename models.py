b94ff0e235cf7072484cfbe3bb475eaaedd8a989
安本 涼 <ae24108@shibaura-it.ac.jp>
2026-03-16 15:33:05 +0900

    add a Initialization process in models.py

--- models.py
+++ models.py
@@ -35,5 +35,25 @@ class Review(Base):
 
     facility = relationship("Facility", back_populates="reviews")
 
+
 #テーブルの作成処理
 Base.metadata.create_all(bind=engine)
+
+session = SessionLocal()
+
+#施設データの初期リスト
+facilities = [
+    Facility(name="おふろの王様 和光店", prefecture="埼玉県", sauna_temp=90, water_temp=16),
+    Facility(name="かるまる池袋", prefecture="東京都", sauna_temp=95, water_temp=14),
+    Facility(name="スパジアムジャポン", prefecture="埼玉県", sauna_temp=92, water_temp=15)
+
+]
+
+#同施設の登録防止
+for data in facilities:
+    existing = session.query(Facility).filter_by(name=data.name).first()
+    if existing is None:
+        session.add(data)
+
+session.commit()
+session.close()
