00f9fb1676e22b30b7c7694c7c92b7cb005884f3
安本 涼 <ae24108@shibaura-it.ac.jp>
2026-03-18 16:32:20 +0900

    delete a Initialization process in models.py

--- models.py
+++ models.py
@@ -43,22 +43,3 @@ class Review(Base):
 
 #テーブルの作成処理
 Base.metadata.create_all(bind=engine)
-
-session = SessionLocal()
-
-#施設データの初期リスト
-facilities = [
-    Facility(name="おふろの王様 和光店", prefecture="埼玉県", sauna_temp=90, water_temp=16),
-    Facility(name="かるまる池袋", prefecture="東京都", sauna_temp=95, water_temp=14),
-    Facility(name="スパジアムジャポン", prefecture="埼玉県", sauna_temp=92, water_temp=15)
-
-]
-
-#同施設の登録防止
-for data in facilities:
-    existing = session.query(Facility).filter_by(name=data.name).first()
-    if existing is None:
-        session.add(data)
-
-session.commit()
-session.close()
